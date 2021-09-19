a1ecd74d9078c3a79ebc91f4a23b968ad7a4cf19
Michael Myrbeck <michaelmyrbeck@gmail.com>
2021-09-19 13:31:31 -0600

    spin that toucan baby

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Mike's Tucan Wonderland</header>
+      <main>
+        <div id="rotator">
+          <img src="https://pgcpsmess.files.wordpress.com/2014/07/image62.jpg" className="App-logo" alt="logo" />
+        </div>
+      </main>
     </div>
   );
 }
