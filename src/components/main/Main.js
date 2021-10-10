32ffcd2d956d14f86f31fd8c597c38e218f85ce1
Michael Myrbeck <michaelmyrbeck@gmail.com>
2021-10-10 14:08:04 -0600

    added slider

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './main.css';
 
+const minSpinSpeed = 1
+const maxSpinSpeed = 6
+
 function Main() {
     const [spinning, setSpinning] = React.useState(false)
+    const [spinSpeed, setSpinSpeed] = React.useState(minSpinSpeed)
 
     return (
         <main>
-            <button onClick={() => { setSpinning(!spinning) }}>Default</button>;
-            <img className={spinning ? 'spin' : ''} alt="toucan" src="https://pgcpsmess.files.wordpress.com/2014/07/image62.jpg" />
+            <button onClick={function () { setSpinning(!spinning) }}>Spin that Toucan</button>
+            <input
+                onChange={(e) => { setSpinSpeed(e.target.value) }}
+                type="range"
+                min={minSpinSpeed}
+                max={maxSpinSpeed}
+                value={spinSpeed}
+            />
+            <img
+                style={spinning ? {
+                    animation: `rotation ${maxSpinSpeed - spinSpeed + 1}s infinite linear`
+                } : null}
+                alt="toucan"
+                src="https://pgcpsmess.files.wordpress.com/2014/07/image62.jpg"
+            />
         </main>
     );
 }
