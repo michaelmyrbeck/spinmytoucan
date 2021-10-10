12a75f4f2ca4043f874b659a11d94d12960b2a1d
Michael Myrbeck <michaelmyrbeck@gmail.com>
2021-10-10 13:41:28 -0600

    add spin to button

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './main.css';
 
 function Main() {
+    const [spinning, setSpinning] = React.useState(false)
+
     return (
         <main>
-            <img src="https://pgcpsmess.files.wordpress.com/2014/07/image62.jpg" className="App-logo" alt="logo" />
+            <button onClick={() => { setSpinning(!spinning) }}>Default</button>;
+            <img className={spinning ? 'spin' : ''} alt="toucan" src="https://pgcpsmess.files.wordpress.com/2014/07/image62.jpg" />
         </main>
     );
 }
